f6198fa86bb9dd0089ab8a85b191d6a53f78ff7d
gabrielaswolff <gabiwolffschmidt@gmail.com>
2024-10-28 21:55:10 -0300

    Configurações deploy

--- src/server.js
+++ src/server.js
@@ -1,20 +1,94 @@
+const upload = require('./multer');
 const express = require('express');
 const cors = require('cors');
 const connection = require('./db_config');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
 
-const porta = 3004;
 const app = express();
+const baseUrl = "http://localhost:3004"
+const porta = process.env.PORT || 3004;
 
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: { 
+      title: 'MINHA API',
+      version: '1.0.0',
+      description: 'A simple Express API with Swagger documentation',
+    },
+    servers: [
+      { url: 'http://localhost:3004' },
+    ],
+  },
+  apis: ['/src/server', 'src/*.js'], 
+};
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
 
 app.use(cors());
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 
- 
-app.listen(porta, () => console.log(`rodando na porta ${porta}`));  
-const upload = require('./multer');
+app.listen(porta, () => console.log(`Servidor rodando na porta ${porta}`));
+
+
+//-----------------------------------------------------------------------------
 
 // CADASTRO DO USUARIO
+
+/**
+    @swagger
+ * /usuario/cadastrar:
+ *   post:
+ *     summary: Cadastro de um novo usuário
+ *     description: Cria um novo usuário no sistema com nome, email, senha e CPF.
+ *     tags:
+ *       - Usuário
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               cpf_number:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Usuário cadastrado com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                 message:
+ *                   type: string
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: integer
+ *                     name:
+ *                       type: string
+ *                     email:
+ *                       type: string
+ *                     password:
+ *                       type: string
+ *       400:
+ *         description: Nome ou email já cadastrado
+ *       500:
+ *         description: Erro no servidor
+ */
 
 app.post('/usuario/cadastrar', (request, response) => {
     const { name, email, password, cpf_number } = request.body;
@@ -54,7 +128,47 @@ app.post('/usuario/cadastrar', (request, response) => {
             });
         });
     });
+
 });
+
+
+
+/**
+ * @swagger
+ * /usuarios/listar:
+ *   get:
+ *     summary: Lista todos os usuários
+ *     description: Retorna uma lista de todos os usuários cadastrados no sistema.
+ *     tags:
+ *       - Usuário
+ *     responses:
+ *       200:
+ *         description: Lista de usuários retornada com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                 message:
+ *                   type: string
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: integer
+ *                       name:
+ *                         type: string
+ *                       email:
+ *                         type: string
+ *                       cpf_number:
+ *                         type: string
+ *       400:
+ *         description: Falha ao buscar usuários
+ */
 
       
 app.get('/usuarios/listar', (request, response) => {
@@ -86,6 +200,55 @@ app.get('/usuarios/listar', (request, response) => {
 
 
 })
+
+/**
+ * @swagger
+ * /usuario/editar/{id}:
+ *   put:
+ *     summary: Edita as informações de um usuário
+ *     description: Atualiza as informações de um usuário existente no sistema, como nome, CPF, email e senha.
+ *     tags:
+ *       - Usuário
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID do usuário a ser editado
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               cpf_number:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Edição realizada com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                 message:
+ *                   type: string
+ *                 data:
+ *                   type: object
+ *       400:
+ *         description: Erro ao editar usuário
+ */
+
 
 app.put('/usuario/editar/:id', (request, response) => {
     const { name, cpf_number, email, password } = request.body;
@@ -119,6 +282,40 @@ app.put('/usuario/editar/:id', (request, response) => {
 });
 
 
+/**
+ * @swagger
+ * /usuario/deletar/{id}:
+ *   delete:
+ *     summary: Deleta um usuário
+ *     description: Remove um usuário do sistema pelo seu ID.
+ *     tags:
+ *       - Usuário
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID do usuário a ser deletado
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Usuário deletado com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                 message:
+ *                   type: string
+ *                 data:
+ *                   type: string
+ *       400:
+ *         description: Erro ao deletar usuário
+ */
+
+
 app.delete('/usuario/deletar/:id', (request, response) => {
     let params = Array(
         request.params.id
@@ -150,6 +347,54 @@ app.delete('/usuario/deletar/:id', (request, response) => {
 })
 
 // LOGIN 
+
+
+/**
+ * @swagger
+ * /login:
+ *   post:
+ *     summary: Realiza o login do usuário
+ *     description: Autentica um usuário pelo email e senha.
+ *     tags:
+ *       - Autenticação
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Login bem-sucedido
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                 message:
+ *                   type: string
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: integer
+ *                     name:
+ *                       type: string
+ *                     email:
+ *                       type: string
+ *                     perfil:
+ *                       type: string
+ *       400:
+ *         description: Erro no login (senha incorreta ou email não cadastrado)
+ */
+
 
 app.post('/login', (request, response) => {
     let params  = Array(
@@ -198,6 +443,39 @@ app.post('/login', (request, response) => {
 
 // CARRINHO 
 
+
+
+/**
+ * @swagger
+ * /carrinho/adicionar:
+ *   post:
+ *     summary: Adiciona um produto ao carrinho
+ *     description: Adiciona um produto ao carrinho de um usuário ou atualiza a quantidade caso já exista no carrinho.
+ *     tags:
+ *       - Carrinho
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               user_id:
+ *                 type: integer
+ *               product_id:
+ *                 type: integer
+ *               quantity:
+ *                 type: integer
+ *     responses:
+ *       200:
+ *         description: Quantidade atualizada no carrinho
+ *       201:
+ *         description: Produto adicionado ao carrinho
+ *       500:
+ *         description: Erro ao adicionar/atualizar o carrinho
+ */
+
+
 app.post('/carrinho/adicionar', (req, res) => {
     const { user_id, product_id, quantity } = req.body;
 
@@ -227,6 +505,33 @@ app.post('/carrinho/adicionar', (req, res) => {
     });
 });
 
+/**
+ * @swagger
+ * /carrinho/remover/{product_id}/{user_id}:
+ *   delete:
+ *     summary: Remove um produto do carrinho
+ *     description: Remove um produto específico do carrinho de um usuário.
+ *     tags:
+ *       - Carrinho
+ *     parameters:
+ *       - in: path
+ *         name: product_id
+ *         required: true
+ *         description: ID do produto a ser removido
+ *         schema:
+ *           type: integer
+ *       - in: path
+ *         name: user_id
+ *         required: true
+ *         description: ID do usuário
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Produto removido com sucesso
+ *       500:
+ *         description: Erro ao remover o produto
+ */
 
 
 app.delete('/carrinho/remover/:product_id/:user_id', (req, res) => {
@@ -242,6 +547,35 @@ app.delete('/carrinho/remover/:product_id/:user_id', (req, res) => {
 });
 
 
+/**
+ * @swagger
+ * /carrinho/editar:
+ *   put:
+ *     summary: Edita a quantidade de um produto no carrinho
+ *     description: Atualiza a quantidade de um produto já existente no carrinho.
+ *     tags:
+ *       - Carrinho
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               user_id:
+ *                 type: integer
+ *               product_id:
+ *                 type: integer
+ *               quantity:
+ *                 type: integer
+ *     responses:
+ *       200:
+ *         description: Quantidade atualizada com sucesso
+ *       500:
+ *         description: Erro ao atualizar a quantidade
+ */
+
+
 app.put('/carrinho/editar', (req, res) => {
     const { user_id, product_id, quantity } = req.body;
 
@@ -253,6 +587,51 @@ app.put('/carrinho/editar', (req, res) => {
         res.status(200).json({ success: true, message: 'quantidade atualizada! sucesso' });
     });
 });
+
+
+/**
+ * @swagger
+ * /carrinho/{user_id}:
+ *   get:
+ *     summary: Lista os produtos no carrinho de um usuário
+ *     description: Retorna os produtos no carrinho de um usuário específico.
+ *     tags:
+ *       - Carrinho
+ *     parameters:
+ *       - in: path
+ *         name: user_id
+ *         required: true
+ *         description: ID do usuário
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Produtos listados com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: integer
+ *                       name:
+ *                         type: string
+ *                       price:
+ *                         type: number
+ *                       image:
+ *                         type: string
+ *                       quantity:
+ *                         type: integer
+ *       500:
+ *         description: Erro ao listar produtos
+ */
 
 
 app.get('/carrinho/:user_id', (req, res) => {
@@ -276,6 +655,52 @@ app.get('/carrinho/:user_id', (req, res) => {
 
 
 // COMPRA 
+
+/**
+ * @swagger
+ * /verificarCompra:
+ *   post:
+ *     summary: Verificar credenciais do usuário para compra.
+ *     description: Verifica se o email e senha fornecidos correspondem a um usuário válido no sistema.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 description: O email do usuário.
+ *               password:
+ *                 type: string
+ *                 description: A senha do usuário.
+ *     responses:
+ *       200:
+ *         description: Usuário verificado com sucesso.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                 message:
+ *                   type: string
+ *       404:
+ *         description: Email ou senha incorretos.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                 message:
+ *                   type: string
+ *       500:
+ *         description: Erro no servidor.
+ */
 
 app.post('/verificarCompra', (req, res) => {
     const { email, password } = req.body;
@@ -302,6 +727,59 @@ app.post('/verificarCompra', (req, res) => {
 
 
 // CADASTRAR PRODUTO INTERFAE ADMIN
+
+
+
+/**
+ * @swagger
+ * /produto/cadastrar:
+ *   post:
+ *     summary: Cadastrar um novo produto.
+ *     description: Permite que um administrador cadastre um novo produto no sistema, incluindo o envio de uma imagem.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 description: O nome do produto.
+ *               price:
+ *                 type: number
+ *                 description: O preço do produto.
+ *               file:
+ *                 type: string
+ *                 format: binary
+ *                 description: A imagem do produto.
+ *               description:
+ *                 type: string
+ *                 description: A descrição do produto.
+ *     responses:
+ *       201:
+ *         description: Produto cadastrado com sucesso.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                 message:
+ *                   type: string
+ *       400:
+ *         description: Falha ao cadastrar o produto.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                 message:
+ *                   type: string
+ */
 
 app.post('/produto/cadastrar', upload.single('file'), (request, response) => {
     let params = Array(
@@ -338,6 +816,45 @@ app.post('/produto/cadastrar', upload.single('file'), (request, response) => {
 app.use('/uploads', express.static(__dirname + '\\public'))
 
 
+
+/**
+ * @swagger
+ * /produtos/listar:
+ *   get:
+ *     summary: Listar todos os produtos.
+ *     description: Retorna uma lista de todos os produtos cadastrados no sistema.
+ *     responses:
+ *       200:
+ *         description: Lista de produtos retornada com sucesso.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                 message:
+ *                   type: string
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: integer
+ *                       name:
+ *                         type: string
+ *                       price:
+ *                         type: number
+ *                       image:
+ *                         type: string
+ *                       description:
+ *                         type: string
+ *       400:
+ *         description: Falha ao listar os produtos.
+ */
+
+
 app.get('/produtos/listar', (request, response) => {
     let query = "select * from products";
 
@@ -363,6 +880,45 @@ app.get('/produtos/listar', (request, response) => {
     })
 })
 
+
+
+/**
+ * @swagger
+ * /produto/editar/{id}:
+ *   put:
+ *     summary: Editar um produto.
+ *     description: Permite que um administrador edite as informações de um produto existente.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID do produto a ser editado.
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               price:
+ *                 type: number
+ *               file:
+ *                 type: string
+ *                 format: binary
+ *               description:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Produto atualizado com sucesso.
+ *       400:
+ *         description: Falha ao atualizar o produto.
+ *       404:
+ *         description: Produto não encontrado.
+ */
 
 app.put('/produto/editar/:id', upload.single('file'), (req, res) => {
     const productId = req.params.id;
@@ -392,6 +948,31 @@ app.put('/produto/editar/:id', upload.single('file'), (req, res) => {
         }
     });
 });
+
+
+
+/**
+ * @swagger
+ * /produto/excluir/{id}:
+ *   delete:
+ *     summary: Excluir um produto.
+ *     description: Remove um produto do sistema pelo ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID do produto a ser excluído.
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Produto excluído com sucesso.
+ *       400:
+ *         description: Falha ao excluir o produto.
+ *       404:
+ *         description: Produto não encontrado.
+ */
+
 
 app.delete('/produto/excluir/:id', (request, response) => {
     const productId = request.params.id;
@@ -430,3 +1011,4 @@ app.delete('/produto/excluir/:id', (request, response) => {
         }
     });
 });
+
